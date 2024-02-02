2728ccb08aa887011f78627919ad82af4490622a
jamescannella <info@jamescannella.com>
2024-02-02 02:41:03 -0700

    update 8

--- app.jsx
+++ app.jsx
@@ -47,9 +47,9 @@ const DEFAULT_THEME = {
 const INITIAL_VIEW_STATE = {
   longitude: -87.63476,
   latitude: 41.87516,
-  zoom: 13.47,
-  pitch: -36.8,
-  bearing: 72
+  zoom: 14.47,
+  pitch: 72,
+  bearing: -36.8
 };
 
 const MAP_STYLE = 'https://api.maptiler.com/maps/cb574b7a-7703-4700-b037-40d45038440c/style.json?key=N8tRase5efDDDNAZ6tfz';
