10e007aeefe4bacc0b0221fae2236375d7878312
jamescannella <info@jamescannella.com>
2024-02-02 10:55:10 -0700

    create single user view

--- app.jsx
+++ app.jsx
@@ -47,9 +47,9 @@ const DEFAULT_THEME = {
 const INITIAL_VIEW_STATE = {
   longitude: -87.62906,
   latitude: 41.8832,
-  zoom: 14.3,
-  pitch: 45,
-  bearing: 50
+  zoom: 15,
+  pitch: 70,
+  bearing: -30
 };
 
 const MAP_STYLE = 'https://api.maptiler.com/maps/cb574b7a-7703-4700-b037-40d45038440c/style.json?key=N8tRase5efDDDNAZ6tfz';
